4f19c6991cb537ebe9e08b78a08ea61b7330b2be
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-10-31 16:46:48 +0000

    create entryData.js file with journal entry objects

---
+++ src/entryData.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    img: {
+      src: "https://nigestyh.github.io/gibralfaro.jpg",
+      alt: "gibralfaro viewpoint",
+    },
+    country: "Spain",
+    title: "Gibralfaro Viewpoint",
+    googleMapLink: "https://maps.app.goo.gl/4QWq8ArYR9ACMY3g7",
+    dates: "08 Jan, 2024 - 11 Jan, 2024",
+    text: "Mount Gibralfaro is a hill located in Málaga in southeast Spain. It is a 130m high foothill of the Montes de Málaga, part of the Cordillera Penibética. At the top of the hill stands the Castle of Gibralfaro overlooking Málaga city and the Mediterranean Sea, and connected by a walled corridor to the Alcazaba of Málaga.",
+  },
+  {
+    img: {
+      src: "https://nigestyh.github.io/fasil-ghebbi.jpg",
+      alt: "fasil ghebbi",
+    },
+    country: "Ethipia",
+    title: "Fasil Ghebbi",
+    googleMapLink: "https://maps.app.goo.gl/PHRxDuUiD7N9243q6",
+    dates: "16 Apr, 2023 - 09 May, 2023",
+    text: "The Fasil Ghebbi is a fortress located in Gondar, Amhara Region, Ethiopia. It was founded in the 17th century by Emperor Fasilides and was the home of Ethiopian emperors. Its unique architecture shows diverse influences including Portuguese, Hindu, and Arab characteristics",
+  },
+  {
+    img: {
+      src: "https://nigestyh.github.io/krka-national-park.jpg",
+      alt: "krka national park",
+    },
+    country: "Croatia",
+    title: "Krka National Park",
+    googleMapLink: "https://maps.app.goo.gl/eK9Jqj6Sm7wtgfJN6",
+    dates: "18 Jun, 2018 - 25 Jun, 2018",
+    text: "Krka National Park is one of the Croatian national parks, named after the river Krka that it encloses. It is located along the middle-lower course of the Krka River in central Dalmatia. It was formed to protect the Krka River and is intended primarily for scientific, cultural, educational, recreational, and tourism activities.",
+  },
+];

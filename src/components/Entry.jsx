70a65bc2a49574823b96c4211943a5216b499081
Ninz11 <148561307+Nigestyh@users.noreply.github.com>
2025-10-31 15:28:08 +0000

    create Entry component with images, location, dates and description

---
+++ src/components/Entry.jsx
@@ -0,0 +1,23 @@
+function Entry() {
+  return (
+    <article>
+      <div>
+        <img src="src/assets/fasil-ghebbi.jpg" />
+      </div>
+      <img src="src/assets/location-pin.png" />
+      <span>Ethiopia</span>
+      <a href="https://maps.app.goo.gl/PHRxDuUiD7N9243q6">
+        View on Google Maps
+      </a>
+      <h2>Fasil Ghebbi</h2>
+      <p>16 Apr, 2023 - 09 May, 2023</p>
+      <p>
+        The Fasil Ghebbi is a fortress located in Gondar, Amhara Region,
+        Ethiopia. It was founded in the 17th century by Emperor Fasilides and
+        was the home of Ethiopian emperors. Its unique architecture shows
+        diverse influences including Portuguese, Hindu, and Arab characteristics
+      </p>
+    </article>
+  );
+}
+export default Entry;
